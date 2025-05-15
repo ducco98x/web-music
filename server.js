49ebb624d38f4f7b59a9beb32dd6d4acbfbad8dd
ducco98x <ducco1711@gmail.com>
2025-05-15 21:45:06 +0000

    w

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Server đang chạy!');
-});
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
